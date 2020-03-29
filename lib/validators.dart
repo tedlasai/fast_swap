e27a68b3f6bf58eb7d41afaf57f383d6333c8ea2
saitedla <tedlasai@gmail.com>
2020-03-29 00:12:46 -0600

    Bug Fixes

--- lib/validators.dart
+++ lib/validators.dart
@@ -11,7 +11,8 @@ class Validators {
   static final RegExp _phoneNumberMatchRegExp = RegExp(r'^[\d ()x\+-]*$');
   static final RegExp _usernameAlphanumericRegExp = RegExp(r'^[a-zA-Z0-9_]+$');
   static final RegExp _handleMatchRegExp = RegExp(r'^[a-zA-Z0-9_\.-]*$');
-  static final RegExp _emailMatchRegExp = RegExp(r'^[a-zA-z0-9\._%+-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,}$');
+  static final RegExp _emailMatchRegExp =
+      RegExp(r'^[a-zA-z0-9\._%+-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,}$');
 
   static _usernameEndingChar(String username) {
     return username.endsWith("_");
@@ -22,7 +23,9 @@ class Validators {
   }
 
   static _username2AdjacentSpecialCharsRegExp(String username) {
-    return username.contains('__') || username.contains('..') || username.contains('--');
+    return username.contains('__') ||
+        username.contains('..') ||
+        username.contains('--');
   }
 
   static final RegExp _passwordRegExp = RegExp(
@@ -45,8 +48,9 @@ class Validators {
     } else
       return "VALID";
   }
+
   static isValidEmail(String email) {
-    if(email == "") {
+    if (email == "") {
       return "VALID";
     } else if (!_usernameStartUnderscoreCharRegExp.hasMatch(email)) {
       return "Email can't start with special character.";
@@ -65,24 +69,20 @@ class Validators {
   static isValidPhoneNumber(String number) {
     if (!_phoneNumberMatchRegExp.hasMatch(number)) {
       return "Only numbers, hyphens and spaces.";
-    } else if(number == "") {
+    } else if (number == "") {
       return "VALID";
-    } else if(hasMiddlePlus(number)) {
+    } else if (hasMiddlePlus(number)) {
       return "Invalid + sign";
-    } else if(getNumbersFromPhoneNumber(number).length < 4) {
+    } else if (getNumbersFromPhoneNumber(number).length < 4) {
       return "Please enter at least 4 digits";
-    } else if(getNumbersFromPhoneNumber(number).length > 20) {
+    } else if (getNumbersFromPhoneNumber(number).length > 20) {
       return "Please enter less than 20 digits";
     } else
       return "VALID";
   }
 
   static isValidHandle(String handle) {
-    if (!_usernameStartUnderscoreCharRegExp.hasMatch(handle)) {
-      return "Handle can't start with special character.";
-    } else if (_usernameEndingChar(handle)) {
-      return "Handle can't end with special character.";
-    } else if (_usernameContainsStar(handle)) {
+    if (_usernameContainsStar(handle)) {
       return "Handle can't have * symbol";
     } else if (_username2AdjacentSpecialCharsRegExp(handle)) {
       return "Handle can't have 2 adjacent underscores.";
@@ -106,5 +106,4 @@ class Validators {
   static String getNumbersFromPhoneNumber(String phoneNumber) {
     return phoneNumber.replaceAll(_onlyNumbers, "");
   }
-
 }
